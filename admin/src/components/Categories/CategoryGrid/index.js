e8179ace508c97e85c219724f2da4b2ae0cd76e8
Hardik Modha <hardik@technopers.com>
2024-04-20 05:50:33 +0530

    feat: categories table view selection

--- admin/src/components/Categories/CategoryGrid/index.js
+++ admin/src/components/Categories/CategoryGrid/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import CategoryCard from '../CategoryCard';
-import {Grid, GridItem} from '@strapi/design-system/Grid';
+import {GridItem} from '@strapi/design-system/Grid';
 import {Button} from '@strapi/design-system/Button';
 import {Typography} from '@strapi/design-system/Typography';
 import styled from 'styled-components';
 import {FormattedMessage} from 'react-intl';
 import getTrad from '../../../utils/getTrad';
 import {useBigCommerceFields} from '../../../contexts/BigCommerceFields';
+import {BaseCheckbox, Stack, Table, Tbody, Td, Th, Thead, Tr, VisuallyHidden} from "@strapi/design-system";
 
 const ViewMoreGridItem = styled(GridItem)`
   height: 100%;
@@ -42,48 +42,62 @@ const CategoryGrid = () => {
     handleChange,
     previewAmount,
     formattedValue,
+    fetchNextPage,
   } = useBigCommerceFields();
 
   const sortedCategories = sortCategories(categories);
 
-  return (
-    <Grid gap={2}>
-      {sortedCategories.slice(0, 3).map((category) => (
-        <GridItem key={category.id} col={3}>
-          <CategoryCard
-            onChange={() => handleChange(category)}
-            selected={isSelected(category)}
-            title={category.name}
-            image={category.image_url}
-            categoryId={category.id}
+  return <div style={{height: '26rem', overflow: 'auto'}}>
+    <Table colCount={1} rowCount={1}>
+      <Thead>
+        <Tr>
+          <Th>
+            <VisuallyHidden>Actions</VisuallyHidden>
+          </Th>
+          <Th>
+            <Typography variant="sigma">ID</Typography>
+          </Th>
+          <Th>
+            <Typography variant="sigma">Name</Typography>
+          </Th>
+          <Th>
+            <Typography variant="sigma">URL</Typography>
+          </Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {categories.map(entry => <Tr key={entry.id}>
+          <Td>
+            <BaseCheckbox value={isSelected(entry)}
+                          onValueChange={() => {
+                            handleChange(entry)
+                          }}
+                          name={`category_${entry.id}`}
+                          aria-label={`Select ${entry.name}`}/>
+          </Td>
+          <Td>
+            <Typography textColor="neutral800">{entry.id}</Typography>
+          </Td>
+          <Td>
+            <Typography textColor="neutral800">{entry.name}</Typography>
+          </Td>
+          <Td>
+            <Typography textColor="neutral800">{entry.custom_url?.url}</Typography>
+          </Td>
+        </Tr>)}
+      </Tbody>
+    </Table>
+    {hasNextPage && (
+      <Stack marginTop={6} horizontal justifyContent="center">
+        <Button variant="tertiary" onClick={fetchNextPage}>
+          <FormattedMessage
+            id={getTrad('onents.CategoryPicker.load-more')}
+            defaultMessage="Load more"
           />
-        </GridItem>
-      ))}
-      {(categories.length > 3 || hasNextPage) && (
-        <ViewMoreGridItem col={3}>
-          <ViewMoreButton variant="tertiary" onClick={togglePicker}>
-            <span>
-              <FormattedMessage
-                id={getTrad('components.CategoryGrid.view-more')}
-                defaultMessage="View more"
-              />
-            </span>
-            <Typography variant="pi" textColor="neutral300">
-              {formattedValue?.length > previewAmount && (
-                <Typography variant="pi" textColor="neutral300">
-                  <FormattedMessage
-                    id="components.CategoryGrid.moreSelected"
-                    defaultMessage="{amount} more selected"
-                    values={{amount: formattedValue.length - previewAmount}}
-                  />
-                </Typography>
-              )}
-            </Typography>
-          </ViewMoreButton>
-        </ViewMoreGridItem>
-      )}
-    </Grid>
-  );
+        </Button>
+      </Stack>
+    )}
+  </div>
 };
 
 export default CategoryGrid;
